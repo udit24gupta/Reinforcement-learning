37fedf2c68d46de84a3317e6258c84d654530b9f
udit24gupta <102501630+udit24gupta@users.noreply.github.com>
2023-11-20 19:52:58 +0530

    Add files via upload

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,57 +1,43 @@
-import numpy as np
+import random
+import matplotlib.pyplot as plt
 
-CARD_VALUES = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+def estimate_pi(num_samples):
+    inside_circle = 0
 
-def calculate_hand_value(hand):
-    """Calculate the value of a blackjack hand."""
-    value = sum(CARD_VALUES[card] for card in hand)
+    for _ in range(num_samples):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
 
-    num_aces = hand.count('A')
-    while value > 21 and num_aces:
-        value -= 10
-        num_aces -= 1
-    return value
+        distance = x**2 + y**2
 
-def play_blackjack(policy, num_episodes=100000):
-    """Simulate blackjack games using the Monte Carlo method."""
-    state_action_values = {}
-    state_visit_counts = {}
+        if distance <= 1:
+            inside_circle += 1
 
-    for _ in range(num_episodes):
-        player_hand = ['A', '2']
-        dealer_hand = ['K']
-        while calculate_hand_value(player_hand) < 12:
-            player_hand.append(np.random.choice(list(CARD_VALUES.keys())))
-        
-        while calculate_hand_value(dealer_hand) < 17:
-            dealer_hand.append(np.random.choice(list(CARD_VALUES.keys())))
+    return (inside_circle / num_samples) * 4
 
-        player_value = calculate_hand_value(player_hand)
-        dealer_value = calculate_hand_value(dealer_hand)
+def plot_points_in_circle(num_samples):
+    inside_x = []
+    inside_y = []
+    outside_x = []
+    outside_y = []
 
-        if player_value > 21 or (dealer_value <= 21 and dealer_value >= player_value):
-            reward = -1  
-        elif player_value == dealer_value:
-            reward = 0 
+    for _ in range(num_samples):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+
+        distance = x**2 + y**2
+
+        if distance <= 1:
+            inside_x.append(x)
+            inside_y.append(y)
         else:
-            reward = 1   
-        state = (calculate_hand_value(player_hand), dealer_hand[0], 'usable' if 'A' in player_hand else 'not-usable')
-        action = policy(state)
-        state_action = (state, action)
+            outside_x.append(x)
+            outside_y.append(y)
 
-        state_visit_counts[state_action] = state_visit_counts.get(state_action, 0) + 1
-        state_action_values[state_action] = state_action_values.get(state_action, 0) + (reward - state_action_values.get(state_action, 0)) / state_visit_counts[state_action]
+    plt.scatter(inside_x, inside_y, color='blue', marker='.')
+    plt.scatter(outside_x, outside_y, color='red', marker='.')
+    plt.show()
+num_samples = 100000
+estimated_pi = estimate_pi(num_samples)
+print(f"Estimated value of pi using {num_samples} samples: {estimated_pi}")
 
-    return state_action_values
-
-def simple_policy(state):
-    """A simple policy for the player."""
-    player_value, dealer_card, usable_ace = state
-    return 'hit' if player_value < 20 else 'stick'
-
-if __name__ == "__main__":
-    policy = simple_policy
-    state_action_values = play_blackjack(policy)
-
-    for key, value in state_action_values.items():
-        print(f"{key}: {value}")
